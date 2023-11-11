0e3ee7a728698a8f264425efbf9cfd0a68defc60
daviszung <daviszung@gmail.com>
2023-11-11 16:27:05 -0500

    lower difficulty to 41 or 46 empty cells

--- src/controls.ts
+++ src/controls.ts
@@ -9,6 +9,7 @@ export type Val = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
 
 export let board: Board = []
 
+
 // Taking ~48ms typically. Takes 60ms or more on first request 
 export async function getBoard() {
 
@@ -17,6 +18,9 @@ export async function getBoard() {
         const data = await response.json()
 
         board = data.newboard.grids[0].value
+        const solution = data.newboard.grids[0].solution
+
+        board = adjustDifficulty(board, solution)
 
         fillBoard(board)
 
@@ -94,4 +98,40 @@ export async function paintSquare(row: number, col: number, val: number) {
     }
 
     return;
+}
+
+// An easy sudoku will have 41 empty squares
+// A medium level sudoku will have 46 empty squares
+// I just want to use easy and medium difficulty for now
+function adjustDifficulty(board: Board, solution: Board) {
+
+    let desiredEmptySquares = Math.random() < .5 ? 41 : 46
+
+    let emptySquares = 0;
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            if (board[i][j] === 0) {
+                emptySquares += 1;
+            }
+        }
+    }
+
+    let squaresToFill = emptySquares - desiredEmptySquares;
+
+    while (squaresToFill > 0) {
+
+        const randomNum = Math.floor(Math.random() * (81 - 1 + 1)) + 1;
+        const row = Math.floor((randomNum - 1) / 9);
+        const col = (randomNum - 1) % 9;
+
+        if (board[row][col] === 0) {
+            board[row][col] = solution[row][col];
+            squaresToFill -= 1;
+        }
+
+    }
+
+    return board;
+
 }
